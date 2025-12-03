cce1153937eb0b4622cc06abb8a17523de0ded56
Maoratt2007 <maoratt2007@gmail.com>
2025-12-03 17:33:35 -0500

    Add initial menu system

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
-int main(){
-	printf("hello world!");
+void banner()
+{
+	printf("------------------------------------\n");
+	printf("         ARP FORGE TOOLKIT         \n");
+	printf("------------------------------------\n");
+}
+
+void show_menu()
+{
+	printf("1 - Offensive Mode\n");
+	printf("2 - ARP Monitor\n");
+	printf("3 - Exit\n");
+}
+
+
+int main()
+{
+	char option = 0;
+	banner();
+
+	while(option != '3')
+	{
+		show_menu();
+		option = getchar();
+		getchar();
+		printf("User entered: %c\n",option);
+	}
+
+	return 0;
 }
